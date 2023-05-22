cc00262965986f9ea8b266ee1abf0ac41ae8db8a
noetic-mujtaba <mujtaba@noeticworld.com>
2023-05-22 01:22:46 -0400

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,13 @@ DIR='/home/mujtaba/deploymentTask/'
 DIRLOG="/var/log/apps/deploymentTask/"
 IMAGE='deploymentTask'
 echo "Pull request from GitHub repo to ~${APPNAME} directory ..." && echo "" && \
-[ ! -d "${DIR}" ] && sudo mkdir -p "${DIR}"
-[ ! -d "${DIRLOG}" ] && sudo mkdir -p "${DIRLOG}"
+[ ! -d "${DIR}" ] && mkdir -p "${DIR}"
+[ ! -d "${DIRLOG}" ] && mkdir -p "${DIRLOG}"
 cd ${DIR}
-git stash && sudo git pull origin main &&
+git stash && git pull origin main &&
 firewall-cmd --permanent --add-port=10019/tcp &&
 firewall-cmd --reload &&
-if [ $( sudo docker ps -a | grep ${IMAGE} | wc -l ) -gt 0 ]; then
+if [ $( docker ps -a | grep ${IMAGE} | wc -l ) -gt 0 ]; then
 echo "container ~${IMAGE} already exists"
 echo "Stopping ${IMAGE}_c from production environment ..." && echo "" && \
 docker stop ${IMAGE}_c  &&
@@ -23,7 +23,7 @@ docker rmi -f ${IMAGE}
 else
 echo "container ~${IMAGE} does not exist"
 fi
-mvn clean install &&
+/opt/apache-maven-3.9.2/bin/mvn clean install &&
 echo "Creating Build for newly pulled code ..." && echo "" && \
 docker build -t ${IMAGE} . &&
 echo "Deploying the container ${IMAGE}_c ..." && echo "" && \
